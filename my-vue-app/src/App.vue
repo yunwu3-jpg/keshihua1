df0a2661e4ed472d7ab731b92a90305155ede825
yunwu3-jpg <2745459915@qq.com>
2025-05-13 15:00:32 +0800

    Create App.vue

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
+</template>
+
+<script setup>
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #f5f7fa;
+}
+</style>
